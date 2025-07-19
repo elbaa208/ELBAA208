3c6c6436124fb3c01f79e186f2f5aa24a976ecb5
elbaa208 <elbaa208@gmail.com>
2025-07-19 15:58:26 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+export default defineConfig({
+  base: "/ELBAA208/",
+  plugins: [react()],
+})
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
